5c6b72096c8b9b0937c44a361070633aa4201da6
harshzalavadiya <harsh@strandls.com>
2020-09-22 22:33:15 +0530

    :recycle: Updated infobar UI

--- src/components/map/infobar/index.tsx
+++ src/components/map/infobar/index.tsx
@@ -1,4 +1,17 @@
-import { CloseButton, IconButton, useDisclosure } from "@chakra-ui/core";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  CloseButton,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Text,
+  useDisclosure
+} from "@chakra-ui/core";
 import styled from "@emotion/styled";
 import React from "react";
 
@@ -13,7 +26,9 @@ const TabContainer = styled.div`
   max-width: 24rem;
   background: white;
   height: 100%;
-  > .sidebar-tabs,
+  overflow: auto;
+  z-index: 4;
+  s > .sidebar-tabs,
   [role="tabpanel"] {
     display: flex;
     flex-direction: column;
@@ -22,20 +37,41 @@ const TabContainer = styled.div`
 `;
 
 export default function InfobarPanel() {
-  const { infobarData } = useLayers();
-  const { isOpen, onToggle, onClose } = useDisclosure({ defaultIsOpen: true });
+  const { infobarData = [] } = useLayers();
+  const { isOpen, onToggle, onClose } = useDisclosure(true);
 
   return isOpen ? (
     <TabContainer>
       <CloseButton m={1} onClick={onClose} position="absolute" right="0" />
-      <pre>{JSON.stringify(infobarData, null, 2)}</pre>
+      <Accordion defaultIndex={[0]} allowMultiple={true}>
+        {infobarData.map(({ properties = {}, layer }) => (
+          <AccordionItem>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                {layer?.source}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+              {Object.entries(properties).map(([k, v]) => (
+                <FormControl key={k} mb={4}>
+                  <FormLabel fontSize="sm" htmlFor={k} color="gray.500">
+                    {k}
+                  </FormLabel>
+                  <Text id={k}>{v}</Text>
+                </FormControl>
+              ))}
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </TabContainer>
   ) : (
     <IconButton
       position="absolute"
-      top={0}
+      top={136}
       right={0}
-      m={4}
+      m={2}
       bg="white"
       size="sm"
       variant="outline"
